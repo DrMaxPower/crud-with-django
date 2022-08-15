b0eac79d0fd333c5ded5e84935c43fce040cf2ee
DrMaxPower <ni.fe@icloud.com>
2022-08-15 12:35:32 +0000

    created model for booking

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+    """ Staff booking table for costumers manual through admin """
+    list_filter = ('guests', 'date')
+    summmernote_fields = ('info',)
+
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Booking(models.Model):
+    """
+    data information from costumers about booking table
+    """
+
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, blank=True)
+    guests = models.PositiveSmallIntegerField()
+    date = models.DateTimeField()
+    info = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f"{self.fname} {self.lname} day: {self.date}"
