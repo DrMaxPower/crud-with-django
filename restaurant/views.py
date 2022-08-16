c4ea9d7564c128fe2cf3334f20b676b898129879
DrMaxPower <ni.fe@icloud.com>
2022-08-16 20:20:32 +0000

    added allauth

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render 
-from django.views import generic, View
+from django.shortcuts import render, redirect 
 from .models import Booking
 from .forms import BookingForm
+from django.contrib import messages
+
 
 
 def home(request):
@@ -9,11 +10,34 @@ def home(request):
     return render(request, 'index.html', {'all':all_booking})
 
 
+def restaurant(request):
+    return render(request, 'restaurant.html', {})
+
+
 def booking(request):
     if request.method == 'POST':
         form = BookingForm(request.POST or None)
         if form.is_valid():
             form.save()
-        return render(request, 'booking.html', {})
+        else:
+            fname = request.POST['fname']
+            lname = request.POST['lname']
+            email = request.POST['email']
+            guests = request.POST['guests']
+            date = request.POST['date']
+            info = request.POST['info']
+            messages.success(request,
+            ('You need set name and time for booking to complete.'))
+            return render(request, 'booking.html', {
+                'fname': fname,
+                'lname': lname,
+                'email': email,
+                'guests': guests,
+                'date': date,
+                'info': info,
+            })
+        messages.success(request,
+            ('Booking a table was successfully submitted.'))
+        return redirect('restaurant')
     else:
         return render(request, 'booking.html', {})
