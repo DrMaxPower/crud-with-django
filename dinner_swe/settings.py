a2cb372ce4c1c2bf7f37780f2abe39030c5e97fc
DrMaxPower <ni.fe@icloud.com>
2022-08-22 14:51:44 +0000

    added back to DEBUG False and X_FRAME_OPTIONS

--- dinner_swe/settings.py
+++ dinner_swe/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# X_FRAME_OPTIONS = 'SAMEORIGION'
+X_FRAME_OPTIONS = 'SAMEORIGION'
 
 ALLOWED_HOSTS = ['sweden-restaurant.herokuapp.com', 'localhost']
 
